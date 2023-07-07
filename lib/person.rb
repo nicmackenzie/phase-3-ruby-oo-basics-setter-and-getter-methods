a8db0e0424c1e181c20c1c01aa8c62109dbd1338
nicmackenzie <nicmackenzie@live.com>
2023-07-07 06:32:21 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    def name=(value)
+        @name = value
+    end
 
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
